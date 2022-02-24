f4bfb9433eb18c59eb5f354b89f4e0f07502b3b5
fergusch <27470183+fergusch@users.noreply.github.com>
2022-02-23 20:03:56 -0600

    add tag pages

--- _plugins/site_generator.rb
+++ _plugins/site_generator.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'set'
 require 'xxhash'
 
 module Jekyll
@@ -41,16 +42,35 @@ module Jekyll
     class SiteGenerator < Generator
         def generate(site)
 
+            tags = Set.new
+
             site.data['photos'].each do |photo_data|
                 # add auto-generated metadata to photos
                 photo_data['id'] = Jekyll::get_photo_hash(photo_data)
                 photo_data['date'] = Jekyll::get_photo_date(photo_data)
+
+                # add tags to tag set
+                photo_data['tags'].each do |tag|
+                    tags << tag
+                end
 
                 # create photo page
                 photo_page = Jekyll::PhotoPage.new(
                     site, site.source, @dir, photo_data
                 )
                 site.pages << photo_page
+            end
+
+            # generate tag page URLs
+            site.data['tags'] = tags.to_a
+            site.data['tag_urls'] = tags.to_a.map { |tag| [tag, tag.downcase.gsub(' ', '-').gsub('(', '').gsub(')', '')]}.to_h
+
+            site.data['tags'].each do |tag|
+                tagged_photos = site.data['photos'].select { |photo| photo['tags'].include?(tag) }
+                tag_page = Jekyll::CollectionPage.new(
+                    site, site.source, @dir, "Tagged: #{tag}", "/tag/#{site.data['tag_urls'][tag]}/index.md", tagged_photos
+                )
+                site.pages << tag_page
             end
 
             # create "all photos" page
