ecda96d261d4a5fd8edf16a3f8fc65d2afaf3101
fergusch <27470183+fergusch@users.noreply.github.com>
2022-02-23 19:37:22 -0600

    add photo pages

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "jekyll", "~> 4.2.1"
+gem "xxhash", "~> 0.4.0"
 gem "webrick", "~> 1.7"
--- _plugins/site_generator.rb
+++ _plugins/site_generator.rb
@@ -1,4 +1,28 @@
+require 'date'
+require 'xxhash'
+
 module Jekyll
+
+    def Jekyll::get_photo_hash(photo_data)
+        XXhash.xxh32(photo_data['file'])
+    end
+
+    def Jekyll::get_photo_date(photo_data)
+        Date.strptime(photo_data['file'], '%Y%m%d').strftime('%B %-d, %Y')
+    end
+
+    class PhotoPage < Page
+        def initialize(site, base, dir, photo_data)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = "/photo/#{photo_data['id']}/index.md"
+            self.process(@name)
+            self.data ||= {}
+            self.data['layout'] = 'photo'
+            self.data['photo'] = photo_data
+        end
+    end
 
     class CollectionPage < Page
         def initialize(site, base, dir, title, url, photo_arr)
@@ -16,6 +40,18 @@ module Jekyll
 
     class SiteGenerator < Generator
         def generate(site)
+
+            site.data['photos'].each do |photo_data|
+                # add auto-generated metadata to photos
+                photo_data['id'] = Jekyll::get_photo_hash(photo_data)
+                photo_data['date'] = Jekyll::get_photo_date(photo_data)
+
+                # create photo page
+                photo_page = Jekyll::PhotoPage.new(
+                    site, site.source, @dir, photo_data
+                )
+                site.pages << photo_page
+            end
 
             # create "all photos" page
             all_photos_page = Jekyll::CollectionPage.new(
