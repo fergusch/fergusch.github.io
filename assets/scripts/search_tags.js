64914045a0c0e32db1d2da929c6b5052f227a466
fergusch <27470183+fergusch@users.noreply.github.com>
2022-03-02 12:49:54 -0600

    search for all words

--- assets/scripts/search_tags.js
+++ assets/scripts/search_tags.js
@@ -1,18 +1,18 @@
-search_box = document.getElementById('search');
+var search_box = document.getElementById('search');
 search_box.onkeyup = function() {
-    results = 0;
-    tags = document.getElementsByTagName('li');
-    for (tag of tags) {
-        if (tag.getElementsByTagName('a')[0].innerHTML.toLowerCase().includes(
-                search_box.value.toLowerCase().replace(/\s{2,}/g, ' ').trim()
-            )) {
-                tag.style.display = 'inline-block';
-                results++;
+    var search_terms = search_box.value.toLowerCase().replace(/\s{2,}/g, ' ').trim().split(' ');
+    var results_len = 0;
+    var tags = document.getElementsByTagName('li');
+    for (var tag of tags) {
+        tag_name = tag.getElementsByTagName('a')[0].innerHTML.toLowerCase();
+        if (search_terms.every((word) => tag_name.includes(word))) {
+            tag.style.display = 'inline-block';
+            results_len++;
         } else {
             tag.style.display = 'none';
         }
     }
-    if (results == 0) {
+    if (results_len == 0) {
         document.getElementById('no-results').style.display = 'block';
     } else {
         document.getElementById('no-results').style.display = 'none';
